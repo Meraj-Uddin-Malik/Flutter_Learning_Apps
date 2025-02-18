0292afc44e80e5f3055ca72321fa248aee01dffc
Meraj-Uddin-Malik <141-2021@lms.indus.edu.pk>
2025-02-18 12:16:23 +0500

    Initial commit

---
+++ lib/MI Card/micard_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class MiCardScreen extends StatelessWidget {
+  const MiCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: SafeArea(child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/profile.png'),
+          ),
+          SizedBox(height: 10.0),
+          Text('Meraj Uddin Malik',
+            style: TextStyle(
+              fontSize: 14.0,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Pacifico',
+              color: Colors.white
+            ),),
+          SizedBox(height: 10.0),
+          Text('Mobile App Developer',
+            style: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Source Code Pro',
+                color: Colors.white
+            ),),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0,),
+            child: ListTile(
+              leading: Icon(Icons.phone, size: 13,
+                color: Colors.deepPurple),
+              title: Text("+92 300 2953272",
+                style: TextStyle(
+                  color: Colors.deepPurple,
+                  fontFamily: 'Sans Code Pro',
+                  fontSize: 13,
+                ),
+              ),
+            )
+          ),
+          Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0,),
+              child: ListTile(
+                leading: Icon(Icons.email, size: 13,
+                  color: Colors.deepPurple),
+                title: Text("merajuddin@gmail.com",
+                  style: TextStyle(
+                    color: Colors.deepPurple,
+                    fontFamily: 'Sans Code Pro',
+                    fontSize: 13,
+                  ),
+                ),
+              )
+          ),
+        ],
+      )
+
+
+      ),
+    );
+  }
+}
